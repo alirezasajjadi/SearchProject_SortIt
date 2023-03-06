610b52f7c7325f9b0820f775450fd34384508c7b
Kourosh Hassanzadeh <kouroshhsz81@gmail.com>
2023-03-06 11:07:21 +0330

    dfs and ids and lds added

--- Search.py
+++ Search.py
@@ -2,10 +2,14 @@ from Solution import Solution
 from Problem import Problem
 from datetime import datetime
 
+# Kourosh Hassanzadeh 9912762552
+# Alireza Sajjadi 9912762596
+
 
 class Search:
     @staticmethod
-    def bfs(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
+    # this method get a first state of Problem and do bfs for find solution if no
+    def bfs(prb: Problem) -> Solution:
         # solution is find return None else return the solution
         start_time = datetime.now()
         queue = []
@@ -19,3 +23,60 @@ class Search:
                     return Solution(c, prb, start_time)
                 queue.append(c)
         return None
+
+    @staticmethod
+    # this method get a first state of Problem and do bfs for find solution if no
+    def dfs(prb: Problem) -> Solution:
+        # solution is find return None else return the solution
+        start_time = datetime.now()
+        queue = []
+        dic = {}
+        state = prb.initState
+        key = state.__hash__()
+        dic[key] = state
+        queue.append(state)
+        while len(queue) > 0:
+            state = queue.pop()
+            key = state.__hash__()
+            dic[key] = state
+            neighbors = prb.successor(state)
+            for c in neighbors:
+                if prb.is_goal(c):
+                    return Solution(c, prb, start_time)
+                if c.__hash__() not in dic:
+                    queue.append(c)
+        return None
+
+    @staticmethod
+    # this method get a first state of Problem and do bfs for find solution if no
+    def lds(prb: Problem, depth) -> Solution:
+        # solution is find return None else return the solution
+        start_time = datetime.now()
+        queue = []
+        dic = {}
+        state = prb.initState
+        key = state.__hash__()
+        dic[key] = state
+        queue.append(state)
+
+        while len(queue) > 0:
+            state = queue.pop()
+            key = state.__hash__()
+            dic[key] = state
+            neighbors = prb.successor(state)
+
+            for c in neighbors:
+                if prb.is_goal(c):
+                    return Solution(c, prb, start_time)
+                if c.__hash__() not in dic and c.g_n <= depth:
+                    queue.append(c)
+        return None
+
+    @staticmethod
+    def ids(pro: Problem):
+        i = 0
+        while True:
+            result = Search.lds(pro, i)
+            i += 1
+            if result is not None:
+                return result
